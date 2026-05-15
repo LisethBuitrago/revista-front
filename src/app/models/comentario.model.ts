530ecbf8088a118e2eb3f4c8818509d4ea506fc9
avillarreala <tatikoavila23@gmail.com>
2026-05-14 20:39:44 -0500

    models

---
+++ src/app/models/comentario.model.ts
@@ -0,0 +1,7 @@
+export interface ComentarioModel {
+  id:number;
+  texto:string;
+  fechaComentario:Date;
+  publicacionId:number;
+  editorId:number;
+}
---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,7 @@
+export interface UsuarioModel {
+  id:number;
+  nombre:string;
+  correo:string;
+  contrasenia:string;
+  rol:string;
+}
